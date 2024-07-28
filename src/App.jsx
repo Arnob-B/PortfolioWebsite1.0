82f366bd264a6f5803e4b7b700367f088211b6c8
Arnob-B <arnobbhakta00@gmail.com>
2024-07-28 18:48:16 +0530

    fixing the darkmode issue

--- src/App.jsx
+++ src/App.jsx
@@ -18,42 +18,42 @@ function Maincomp() {
   return (
     // main container file
     <div className={(bgcol ? "bg-black text-white" : " bg-white text-black") + " flex flex-col"}>
-      <div className='z-10 fixed left-1 top-1 md:hidden'>
-        {/* navbar toggler vertical slide-in*/}
-        <button className='w-5 h-5 bg-green-700 rounded text-green-500 active:text-green-700 bg-inherit object-contain' onClick={swtichtog}>
-          <img src={(tog) ? main.misc.togglebutton2 : main.misc.togglebutton1} alt="test" />
-        </button>
-      </div>
-      <div className='bg-inherit'>
-        {/* navbar pannel */}
-        <Topnav
-          bcol={bgcol}
-          sbgcol={sbgcol}
-          tog={tog}
-          prop={main.navbar}
-        ></Topnav>
-      </div>
-      {/*  the main contents of the body begins */}
-      <div className='xl:px-60 lg:px-40 md:px-10 sm:px-2 '>
-        <ShortIntro
-          prop={main.shortintro}
-          bgcol={bgcol}
-        ></ShortIntro>
-        <Maincontent
-          props={main.maincontent}
-        ></Maincontent>
-        <Mainworkcards
-          carddetails={main.projects}
-        ></Mainworkcards>
-        <WorkExpMain
-          proffesionalcarrierdetails={main.status.p1}
-          educationalcarrierdetails={main.status.e1}
-        />
-        <Mainquotes
-          props={main.reviews}
-        >
-        </Mainquotes>
-      </div>
+    <div className='z-10 fixed left-1 top-1 md:hidden'>
+    {/* navbar toggler vertical slide-in*/}
+    <button className='w-5 h-5 bg-green-700 rounded text-green-500 active:text-green-700 bg-inherit object-contain' onClick={swtichtog}>
+    <img src={(tog) ? main.misc.togglebutton2 : main.misc.togglebutton1} alt="test" />
+    </button>
+    </div>
+    <div className='bg-inherit'>
+    {/* navbar pannel */}
+    <Topnav
+    bgcol={bgcol}
+    sbgcol={sbgcol}
+    tog={tog}
+    prop={main.navbar}
+    ></Topnav>
+    </div>
+    {/*  the main contents of the body begins */}
+    <div className='xl:px-60 lg:px-40 md:px-10 sm:px-2 '>
+    <ShortIntro
+    prop={main.shortintro}
+    bgcol={bgcol}
+    ></ShortIntro>
+    <Maincontent
+    props={main.maincontent}
+    ></Maincontent>
+    <Mainworkcards
+    carddetails={main.projects}
+    ></Mainworkcards>
+    <WorkExpMain
+    proffesionalcarrierdetails={main.status.p1}
+    educationalcarrierdetails={main.status.e1}
+    />
+    <Mainquotes
+    props={main.reviews}
+    >
+    </Mainquotes>
+    </div>
     </div>
   );
 }
@@ -61,12 +61,12 @@ function Maincomp() {
 const Loading = () => {
   return <div>
     "loading ....."
-  </div>
+    </div>
 }
 function App() {
   return <div>
     <Suspense fallback={<Loading />}><Maincomp /></Suspense>
-  </div>
+    </div>
 
 }
 
